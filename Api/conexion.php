7dc0897f52a8ad81c114d64241248441becaf1a2
Eliclavijo <98434969+Eliclavijo@users.noreply.github.com>
2023-11-07 09:35:07 -0500

    Delete Api directory

--- Api/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-ini_set('display_errors', 1);
-$conn=mysqli_connect("localhost","root","admin","librarypublic_bd");
-$conn->set_charset("utf8");
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-} 
-?>
