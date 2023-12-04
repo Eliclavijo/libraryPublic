2d3d88979e8b2a22ebed05d3ff619d74c812490c
Eliclavijo <98434969+Eliclavijo@users.noreply.github.com>
2023-12-03 21:28:14 -0500

    Update test.php

--- Vendor/bin/phpunit/test.php
+++ Vendor/bin/phpunit/test.php
@@ -1,3 +1,20 @@
- <?php
- use PHPUnit\Framework\TestCase;                          
-?>
+<?php
+use PHPUnit\Framework\TestCase;
+
+class LibrosTest extends TestCase
+{
+    public function testMostrarLibros()
+    {
+        // Creamos una instancia de la clase que contiene el código a probar
+        $libros = new Libros();
+
+        // Obtenemos el resultado de ejecutar el método mostrarLibros()
+        $resultado = $libros->mostrarLibros();
+
+        // Definimos el resultado esperado, que sería el HTML generado por el código
+        $esperado = "<div class=\"cardLibro\"> ... </div>"; // Aquí iría todo el HTML que se espera
+
+        // Comprobamos que el resultado obtenido es igual al esperado
+        $this->assertSame($resultado, $esperado);
+    }
+}
