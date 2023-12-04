626283082c0a2b86b22c6f138256439e7992273e
Eliclavijo <98434969+Eliclavijo@users.noreply.github.com>
2023-12-03 21:36:41 -0500

    Update test.php

--- Vendor/bin/phpunit/test.php
+++ Vendor/bin/phpunit/test.php
@@ -11,8 +11,8 @@ class LibrosTest extends TestCase
         // Obtenemos el resultado de ejecutar el método mostrarLibros()
         $resultado = $libros->mostrarLibros();
 
-        // Definimos el resultado esperado, que sería el HTML generado por el código
-        $esperado = "<div class=\"cardLibro\"> ... </div>"; // Aquí iría todo el HTML que se espera
+        // Definimos el resultado esperado
+        $esperado = "<div class=\"cardLibro\"> ... </div>"; 
 
         // Comprobamos que el resultado obtenido es igual al esperado
         $this->assertSame($resultado, $esperado);
